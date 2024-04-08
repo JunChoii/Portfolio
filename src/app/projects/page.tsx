8c84dec68802ee2d697af2fb6233a3d7e21b94f2
Jun <junability@gmail.com>
2024-04-08 16:24:54 -0700

    routes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,61 @@
 import Navbar from "./../Navbar";
+import Image from "next/image";
 
 export default function Projects() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-between p-4 md:p-24 bg-slate-600">
-      <div className="top flex flex-col md:flex-row items-center justify-center md:justify-start">
-        <h1 className="text-6xl font-bold text-white mb-2 md:mb-0">Projects</h1>
+      <div className="top flex flex-col md:flex-row items-center justify-center md:justify-start grid grid-cols-4">
+        <div className="w-60 h-60 m-4 p-2 bg-slate-400 rounded-lg item-center">
+          <Image
+            src="/volterrathumb.png"
+            alt=""
+            width="300"
+            height="300"
+            layout=""
+            className="object-cover"
+          />
+        </div>
+        <div className="w-60 h-60 m-4 p-2 bg-slate-400 rounded-lg item-center">
+          <Image
+            src="/foodsnapthumb.png"
+            alt=""
+            width="300"
+            height="300"
+            layout=""
+            className="object-cover"
+          />{" "}
+        </div>
+        <div className="w-60 h-60 m-4 p-2 bg-slate-400 rounded-lg item-center">
+          <Image
+            src="/cointrackthumb.png"
+            alt=""
+            width="300"
+            height="300"
+            layout=""
+            className="object-cover"
+          />{" "}
+        </div>
+        <div className="w-60 h-60 m-4 p-2 bg-slate-400 rounded-lg item-center">
+          <Image
+            src="/whitestonethumb.png"
+            alt=""
+            width="300"
+            height="300"
+            layout=""
+            className="object-cover"
+          />{" "}
+        </div>
+
+        <div className="w-60 h-60 m-4 p-2 bg-slate-400 rounded-lg item-center">
+          <Image
+            src="/numberclashthumb.png"
+            alt=""
+            width="300"
+            height="300"
+            layout=""
+            className="object-cover"
+          />{" "}
+        </div>
       </div>
       <div className="bottom">
         <Navbar />
