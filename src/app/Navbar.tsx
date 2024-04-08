63399053cf1f403ec612e35a3a2aa28733eeeabb
Jun <junability@gmail.com>
2024-04-08 15:57:50 -0700

    Add new dependencies and update page layout

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 mx-auto w-full md:w-3/4 lg:w-3/4 xl:w-3/4 rounded-2xl mb-5 p-4 bg-slate-800 text-white flex justify-between">
+      <div className="flex items-center flex-grow justify-evenly">
+        <a href="/" className="">
+          <Image
+            src="/home.png"
+            alt=""
+            width="70"
+            height="50"
+            layout=""
+            className="object-cover rounded-lg p-3 bg-neutral-300"
+          />
+          <p className="mt-2 text-xs flex justify-center">Home</p>
+        </a>
+        <Link href="/aboutme">
+          <Image
+            src="/aboutme.png"
+            alt=""
+            width="70"
+            height="50"
+            layout=""
+            className="object-cover rounded-lg p-3 bg-neutral-300"
+          />{" "}
+          <p className="mt-2 text-xs flex justify-center">About me</p>
+        </Link>
+        <Link href="/projects" className="">
+          <Image
+            src="/projects.png"
+            alt=""
+            width="70"
+            height="50"
+            layout=""
+            className="object-cover rounded-lg p-3 bg-neutral-300"
+          />{" "}
+          <p className="mt-2 text-xs flex justify-center">Projects</p>
+        </Link>
+        <Link href="/contact" className="">
+          <Image
+            src="/phone.png"
+            alt=""
+            width="70"
+            height="50"
+            layout=""
+            className="object-cover rounded-lg p-3 bg-neutral-300"
+          />{" "}
+          <p className="mt-2 text-xs flex justify-center">Contact</p>
+        </Link>
+      </div>
+    </nav>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./../Navbar";
+
+export default function Projects() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-between p-4 md:p-24 bg-slate-600">
+      <div className="top flex flex-col md:flex-row items-center justify-center md:justify-start">
+        <h1 className="text-6xl font-bold text-white mb-2 md:mb-0">Projects</h1>
+      </div>
+      <div className="bottom">
+        <Navbar />
+      </div>
+    </main>
+  );
+}
